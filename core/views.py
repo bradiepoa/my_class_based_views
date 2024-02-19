9a78371cc2881ebfd40b7a5a36a8881f1fd9aa2f
bradiepoa7@gmail.com <bradiepoa7@gmail.com>
2024-02-19 03:04:45 +0300

    listview

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import*
+from django.views.generic import ListView, DetailView,UpdateView
 
 # Create your views here.
+
+class IndexView(ListView):
+    model = Core
+    template_name = 'core/index.html'
