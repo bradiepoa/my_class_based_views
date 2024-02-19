2b5a6229f01ee2ca9e8ac728487efc2174927561
bradiepoa7@gmail.com <bradiepoa7@gmail.com>
2024-02-19 18:03:46 +0300

    single.html

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . models import* 
 
 # Register your models here.
+
+@admin.register(Core)
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields={'slug':('title',),}
